a7f7554fc4b70f1d47c1459106970a4609838b4b
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 15:48:08 +0530

    added the properties to department ENTITY

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -1,4 +1,8 @@
 package com.example.demo.entity;
 
 public class Department {
+    private long departmentID;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
 }
