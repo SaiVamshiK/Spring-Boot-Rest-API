62f884108481a0d7c7b4800e8ba3000ad4db9fbd
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 18:33:34 +0530

    passed call from service layer to repo layer

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -3,6 +3,10 @@ package com.example.demo.service;
 import com.example.demo.entity.Department;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface DepartmentService {
     Department createDepartment(Department department);
+
+    List<Department> getAllDepartments();
 }
--- src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
+++ src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 // We mark beans with @Service to indicate that they're holding the business logic.
 @Service
 public class DepartmentServiceImplementation implements DepartmentService{
@@ -13,5 +15,10 @@ public class DepartmentServiceImplementation implements DepartmentService{
     @Override
     public Department createDepartment(Department department) {
         return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
     }
 }
