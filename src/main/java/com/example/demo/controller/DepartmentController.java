f2de1b10c01e19e00e03d04e988386679558b0d8
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 17:34:08 +0530

    using @RequestBody for converting JSON request data to Department Class data

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
     @RequestMapping("/createDepartment")
-    public Department createDepartment(Department department)
+    public Department createDepartment(@RequestBody Department department)
     {
         return departmentService.createDepartment(department);
     }
