7677fd71b26b6626c0165bab7d52b4f9863449aa
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 16:58:32 +0530

    minor change to @Service

--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
-// We mark beans with @Service to indicate that they're holding the business logic.
-@Service
+
 public interface DepartmentService {
 }
--- src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
+++ src/main/java/com/example/demo/service/DepartmentServiceImplementation.java
@@ -1,4 +1,8 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+// We mark beans with @Service to indicate that they're holding the business logic.
+@Service
 public class DepartmentServiceImplementation implements DepartmentService{
 }
