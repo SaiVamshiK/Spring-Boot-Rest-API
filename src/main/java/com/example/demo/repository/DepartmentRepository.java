52fa2e1a80839014cfe2845bfb5f5e37ba42876f
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 16:53:27 +0530

    adding need for @Service and @Repository

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -4,7 +4,8 @@ import com.example.demo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// here we are extending the built in JpaRepository as the Jpa Repository provides us lot of functions to interact with the database
+// here we are extending the built-in JpaRepository as the Jpa Repository provides us lot of functions to interact with the database
+// @Repository’s job is to catch persistence-specific exceptions and re-throw them as one of Spring’s unified unchecked exceptions.
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 
