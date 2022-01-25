6a490db56b5e1a9f9fdd7f6285f4927e6fe59c29
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 17:14:46 +0530

    create functionality added in controller passed call to the service layer

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,7 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Department;
+import com.example.demo.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
+    @Autowired
+    DepartmentService departmentService;
+    @RequestMapping("/createDepartment")
+    public Department createDepartment(Department department)
+    {
+        return departmentService.createDepartment(department);
+    }
 }
