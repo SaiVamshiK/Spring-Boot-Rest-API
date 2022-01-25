988b43c16d42b806cfa8d417f4485c2f62baf43f
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-25 18:27:25 +0530

    started with R (reading all depts) passed call from controller to service layer

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -3,10 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -18,5 +20,10 @@ public class DepartmentController {
     {
         System.out.println("Create method called");
         return departmentService.createDepartment(department);
+    }
+    @GetMapping("/departments")
+    public List<Department> getAllDepartments()
+    {
+        return departmentService.getAllDepartments();
     }
 }
