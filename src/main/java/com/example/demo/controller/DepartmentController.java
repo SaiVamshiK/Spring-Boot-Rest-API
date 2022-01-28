1fabbc53c45ddf210f98a5f3d39d466b8729c7a1
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-28 10:43:06 +0530

    added logger object

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+    private final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
 
     @PostMapping("/createDepartment")
     public Department createDepartment(@Valid @RequestBody Department department)
