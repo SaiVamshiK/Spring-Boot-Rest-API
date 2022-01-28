542c2e6d7ea05ed62768880eefe7a8eb26d4d251
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-28 11:14:48 +0530

    used @Data in lombok by removing all BOILER PLATE Code

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
@@ -9,9 +10,17 @@ import javax.persistence.Id;
 import javax.validation.constraints.*;
 
 @Entity
+@Data
 public class Department {
     // @Entity specifies that department is the table in data jpa
     // id is the primary key for the department table
+//    @Data is a convenient annotation that combines the features of the following annotations:
+//
+//    @ToString
+//    @EqualsAndHashCode
+//    @Getter
+//    @Setter
+//    @RequiredArgsConstructor
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentID;
@@ -27,55 +36,55 @@ public class Department {
     private String departmentCode;
     // refer this : https://thorben-janssen.com/jpa-generate-primary-keys/ for generating values for primary keys
 
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentID=" + departmentID +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
-
-    public Long getDepartmentID() {
-        return departmentID;
-    }
-
-    public void setDepartmentID(long departmentID) {
-        this.departmentID = departmentID;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public Department(long departmentID, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentID = departmentID;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
+//    @Override
+//    public String toString() {
+//        return "Department{" +
+//                "departmentID=" + departmentID +
+//                ", departmentName='" + departmentName + '\'' +
+//                ", departmentAddress='" + departmentAddress + '\'' +
+//                ", departmentCode='" + departmentCode + '\'' +
+//                '}';
+//    }
+//
+//    public Long getDepartmentID() {
+//        return departmentID;
+//    }
+//
+//    public void setDepartmentID(long departmentID) {
+//        this.departmentID = departmentID;
+//    }
+//
+//    public String getDepartmentName() {
+//        return departmentName;
+//    }
+//
+//    public void setDepartmentName(String departmentName) {
+//        this.departmentName = departmentName;
+//    }
+//
+//    public String getDepartmentAddress() {
+//        return departmentAddress;
+//    }
+//
+//    public void setDepartmentAddress(String departmentAddress) {
+//        this.departmentAddress = departmentAddress;
+//    }
+//
+//    public String getDepartmentCode() {
+//        return departmentCode;
+//    }
+//
+//    public void setDepartmentCode(String departmentCode) {
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department(long departmentID, String departmentName, String departmentAddress, String departmentCode) {
+//        this.departmentID = departmentID;
+//        this.departmentName = departmentName;
+//        this.departmentAddress = departmentAddress;
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department() {
+//    }
 }
